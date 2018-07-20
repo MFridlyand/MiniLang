1a7262c83817d681221174652101cf49d13393c3
misha <quant87@mail.ru>
2018-07-20 23:30:13 +0300

    improve test coverage

--- src/tests/Test1.java
+++ src/tests/Test1.java
@@ -76,5 +76,21 @@ class Test1 {
 			e.printStackTrace();
 		}
 	}
+	
+	@Test
+	void test4() {
+		try {
+			String program = new String(
+					Files.readAllBytes(Paths.get("D:\\misha\\src\\hackerrank\\MiniLang\\ftest2.txt")),
+					StandardCharsets.UTF_8);
+			Interpreter lang = new Interpreter();
+			lang.eval(program);
+			System.out.println("done");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
 
 }
